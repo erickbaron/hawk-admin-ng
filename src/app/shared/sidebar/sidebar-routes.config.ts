f16783340c3d52ab3404e07aab7846b96bedc323
erickbaron <erickbaron123456@gmail.com>
2019-09-11 09:49:57 -0300

    Arrumado menu lateral, adicionados icones Hawk

--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -3,19 +3,28 @@ import { RouteInfo } from './sidebar.metadata';
 export const ROUTES: RouteInfo[] = [
 
     {
-        path: '', title: 'Produto',  icon: 'ft-align-left', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
+        path: '', title: 'Categoria', icon: 'ft-align-left', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
+        submenu: [
+            { path: '/categoria/', title: 'Lista', icon: 'list', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
+            { path: '/categoria/cadastro', title: 'Cadastro', icon: 'plus-square', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
+        ]
+    },
+    {
+        path: '', title: 'Produto', icon: 'ft-align-left', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
         submenu: [
             { path: '/produto/', title: 'Lista', icon: 'list', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
             { path: '/produto/cadastro', title: 'Cadastro', icon: 'plus-square', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
-        ]},
+        ]
+    },
     {
-        path: '', title: 'Estoque',  icon: 'ft-align-left', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
+        path: '', title: 'Estoque', icon: 'ft-align-left', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
         submenu: [
             { path: '/estoque/', title: 'Lista', icon: 'list', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
             { path: '/estoque/cadastro', title: 'Cadastro', icon: 'plus-square', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
-        ]},
-    
-    
+        ]
+    },
+
+
     { path: 'https://pixinvent.com/apex-angular-4-bootstrap-admin-template/documentation', title: 'Documentation', icon: 'ft-folder', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
     { path: 'https://pixinvent.ticksy.com/', title: 'Support', icon: 'ft-life-buoy', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
 
