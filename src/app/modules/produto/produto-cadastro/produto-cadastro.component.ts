80cc6f7c40a8922428ee293478d47b910de20277
Jonatas Adams da Silva <jonatas.adams@gmail.com>
2019-09-12 08:57:29 -0300

    Desenvolvido Botão Editar na tela Produto

--- src/app/modules/produto/produto-cadastro/produto-cadastro.component.ts
+++ src/app/modules/produto/produto-cadastro/produto-cadastro.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProdutoService } from 'app/services/produto.service';
 import { Produto } from 'app/models/produto';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-produto-cadastro',
@@ -9,17 +9,17 @@ import { ActivatedRoute } from '@angular/router';
   styles: []
 })
 export class ProdutoCadastroComponent implements OnInit {
+  returnUrl: string;
 
   produto: Produto = new Produto();
   
-  id: number
   constructor(
     private service: ProdutoService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
-    this.id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.obterPeloId(this.id);
+    this.returnUrl = '/produto'
   }
 
   salvar() {
@@ -31,5 +31,9 @@ export class ProdutoCadastroComponent implements OnInit {
       // erro
       alert("Não foi possível cadastrar")
     })
+  }
+
+  cancelar() {
+    this.router.navigateByUrl(this.returnUrl)
   }
 }
--- src/app/modules/produto/produto-editar/produto-editar.component.ts
+++ src/app/modules/produto/produto-editar/produto-editar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdutoService } from 'app/services/produto.service';
+import { ActivatedRoute } from '@angular/router';
+import { Produto } from 'app/models/produto';
 
 @Component({
   selector: 'app-produto-editar',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ProdutoEditarComponent implements OnInit {
-
-  constructor() { }
+  produto: Produto = new Produto;
+  id: number
+  constructor(
+    private service: ProdutoService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.obterPeloId(this.id);
+  }
+
+  obterPeloId(id) {
+    this.service.obterPeloId(id).subscribe(x => {
+      this.produto = x;
+    })
+  }
+
+
+  alterar(produto) {
+    this.service.alterar(produto).subscribe( x => {
+      alert("Registro Alterado com Sucesso")
+    },
+    error => {
+      alert("Não foi possível alterar")
+    })
   }
 
 }
