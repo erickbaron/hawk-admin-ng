7b0361cf2eaec8b85be4fc1d95e666b535865581
Jonatas Adams da Silva <jonatas.adams@gmail.com>
2019-09-12 10:44:52 -0300

    Correção de erros

--- src/app/models/produto.ts
+++ src/app/models/produto.ts
@@ -6,16 +6,15 @@ import { Empresa } from './empresa';
 export class Produto {
     id: number;
     nome: string;
-    valor_custo: number;
-    promocao: boolean;
-    valor_venda: number;
+    valorCusto: number;
+    valorVenda: number;
     descricao: string;
     peso: number;
     altura: number;
     largura: number;
     comprimento: number;
-    registro_ativo: boolean;
+    registroAtivo: boolean;
     empresaId: Empresa;
     categoriaId: Categoria;
-    avaliacao_produto: AvaliacaoProduto;
+    avaliacaoProduto: AvaliacaoProduto;
 }
