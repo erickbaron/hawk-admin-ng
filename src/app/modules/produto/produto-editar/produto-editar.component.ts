fad33876a5791180997c6b3ec7a2ce9a2c78bc57
Jonatas Adams da Silva <jonatas.adams@gmail.com>
2019-09-13 10:26:46 -0300

    Finalizado Categoria-Editar e adicionado ícones

--- src/app/modules/produto/produto-editar/produto-editar.component.ts
+++ src/app/modules/produto/produto-editar/produto-editar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProdutoService } from 'app/services/produto.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Produto } from 'app/models/produto';
 
 @Component({
@@ -9,13 +9,18 @@ import { Produto } from 'app/models/produto';
   styles: []
 })
 export class ProdutoEditarComponent implements OnInit {
+  returnUrl: string;
+
   produto: Produto = new Produto;
+
   id: number
   constructor(
     private service: ProdutoService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.returnUrl = '/produto'
     this.id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.obterPeloId(this.id);
   }
@@ -34,6 +39,10 @@ export class ProdutoEditarComponent implements OnInit {
     error => {
       alert("Não foi possível alterar")
     })
+  }
+
+  cancelar() {
+    this.router.navigateByUrl(this.returnUrl)
   }
 
 }
