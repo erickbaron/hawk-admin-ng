4c2e7b85d7228a421419613a098ac33ca0ffde8f
erickbaron <erickbaron123456@gmail.com>
2019-10-04 09:13:25 -0300

    Mudanças visuais

--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -2,13 +2,13 @@ import { RouteInfo } from './sidebar.metadata';
 
 export const ROUTES: RouteInfo[] = [
 
-    {
-        path: '', title: 'Categoria', icon: 'ft-align-left', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
-        submenu: [
-            { path: '/categoria/', title: 'Lista', icon: 'list', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
-            { path: '/categoria/cadastro', title: 'Cadastro', icon: 'plus-square', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
-        ]
-    },
+    // {
+    //     path: '', title: 'Categoria', icon: 'ft-align-left', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
+    //     submenu: [
+    //         { path: '/categoria/', title: 'Lista', icon: 'list', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
+    //         { path: '/categoria/cadastro', title: 'Cadastro', icon: 'plus-square', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] },
+    //     ]
+    // },
     {
         path: '', title: 'Produto', icon: 'ft-align-left', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
         submenu: [
@@ -31,6 +31,6 @@ export const ROUTES: RouteInfo[] = [
     },
         
 
-    { path: 'https://pixinvent.com/apex-angular-4-bootstrap-admin-template/documentation', title: 'Documentation', icon: 'ft-folder', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] }
+    // { path: 'https://pixinvent.com/apex-angular-4-bootstrap-admin-template/documentation', title: 'Documentation', icon: 'ft-folder', class: '', badge: '', badgeClass: '', isExternalLink: true, submenu: [] }
 
 ];
